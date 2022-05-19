277d3cb5cdfc9804b11df42f74f9ac0ed1c5c367
Tim Shaker <tim@tmshkr.com>
2022-05-19 13:33:40 -0700

    contact list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import "./styles.css";
 
 import { useState } from "react";
+import Table from "./components/Table";
 
 export default function App() {
   const contactList = [
     { id: 1, name: "Joe", phone: "111-222-3333" },
-    { id: 2, name: "Fred", phone: "222-444-6666" }
+    { id: 2, name: "Fred", phone: "222-444-6666" },
   ];
   const [contacts, setContacts] = useState(contactList);
 
   return (
-    <div className="App">
-      <h1>Manage Contacts</h1>
-      <h2>Contact List</h2>
-      <div>
-        TODO: List Contacts here showing name and phone, with edit button for
-        each contact
-      </div>
+    <div className="App p-5">
+      <Table {...{ contacts }} />
       <h2>Add Contact</h2>
       <div>TODO: Interface to add a contact</div>
     </div>
